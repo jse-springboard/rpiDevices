928bccbd6b24cbde2352028c5b0407e981923e0c
Jordan Eriksen <jordan.eriksen@springboard.pro>
2021-07-30 11:34:17 +0100

    Setup tco8.py example for lunchtime demo

--- Examples/tc08.py
+++ Examples/tc08.py
@@ -1,18 +1,18 @@
-from numpy.lib import utils
-from rpiDevices.Devices.rpi import tc08
+from rpiDevices.rpi import tc08
 
 if __name__ == '__main__':
     # Initiate an instance of the tc08 class to startup the unit
     # Specify the channels used by the instance.
 
-    tc = tc08(channel=[1,2,3])
+    channels = [1,3,5,7]
+
+    tc = tc08(channel=channels)
 
     # tc08.get_temp() returns both the temperatures on the channels specified (as a dict) and the temperature of the cold junction.
     output_temp = tc.get_temp()
 
-    channel1_temp = output_temp[1]
-    channel2_temp = output_temp[2]
-    channel3_temp = output_temp[3]
+    for ch in channels:
+        print(f'Ch {ch} temperature = {output_temp[ch]:.2f}')
 
     # Shutdown unit
     tc.shutdown()
