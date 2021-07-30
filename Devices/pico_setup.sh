72a6ab613a584a6a9a6db9232ad966d54f1243fe
Jordan Eriksen <jordan.eriksen@springboard.pro>
2021-07-30 11:59:54 +0100

    New setup.py file to install required picolog drivers

--- Devices/pico_setup.sh
+++ Devices/pico_setup.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
-
-cd ~
 sudo bash -c 'echo "deb https://labs.picotech.com/debian/ picoscope main" >/etc/apt/sources.list.d/picoscope.list'
 wget -O - https://labs.picotech.com/debian/dists/picoscope/Release.gpg.key | sudo apt-key add -
 wget https://labs.picotech.com/debian/pool/main/libp/libpicohrdl/libpicohrdl_2.0.17-1r1441_armhf.deb
 wget https://labs.picotech.com/debian/pool/main/libu/libusbtc08/libusbtc08_2.0.17-1r1441_armhf.deb
 sudo apt install -q ./libpicohrdl_2.0.17-1r1441_armhf.deb ./libusbtc08_2.0.17-1r1441_armhf.deb -y
 git clone https://github.com/picotech/picosdk-python-wrappers.git
-cd picosdk-python-wrappers
-sudo python3 setup.py install
+cd picosdk-python-wrappers && sudo python3 setup.py install
---
+++ Devices/setup.py
@@ -0,0 +1,5 @@
+import sys, os
+
+if __name__ == '__main__':
+    if str(sys.argv[1]) == 'install':
+        os.system('./pico_setup.sh')
