8e41493cf215d2ce231953a8326c01a9da0a8267
Jordan Eriksen <jordan.eriksen@springboard.pro>
2021-08-16 17:19:36 +0100

    Updated call to all_out

--- Examples/adc24.py
+++ Examples/adc24.py
@@ -1,4 +1,4 @@
-from rpiDevices.Devices.rpi import adc24
+from rpiDevices.rpi import adc24
 
 if __name__ == '__main__':
     # Start adc instance and initiate system
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     adc = adc24(channel={1:[0,10],2:[-0.1,300]})
 
     # Output data from all input channels.
-    data_output, data_times = adc.all_out()
+    data_output, data_times = adc.collect(method='stream',nsamples=10)
 
     channel1_output = {'Time':data_times[1],'Data':data_output[1]}
     channel2_output = {'Time':data_times[2],'Data':data_output[2]}
