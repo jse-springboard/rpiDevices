a634753fa5da92e87efb6f96cdb09c011139506f
Jordan Eriksen <jordan.eriksen@springboard.pro>
2021-08-17 16:26:56 +0100

    Set nsamples default to 1

--- rpi.py
+++ rpi.py
@@ -420,7 +420,7 @@ class adc24:
 
         return values_out, times_out
 
-    def collect(self,method='block',nsamples=4,reset=0):
+    def collect(self,method='block',nsamples=1,reset=0):
         '''
         Collect data from ADC using specified method
         --------------------------------------------
